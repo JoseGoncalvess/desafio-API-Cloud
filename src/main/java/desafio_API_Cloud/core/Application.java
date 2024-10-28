9876fc4400ab1ede904d7264e98005bf2da62abf
José Gonçalves <joseinacio774@gmail.com>
2024-10-28 12:02:45 -0300

    Update Application.java

--- src/main/java/desafio_API_Cloud/core/Application.java
+++ src/main/java/desafio_API_Cloud/core/Application.java
@@ -1,5 +1,7 @@
 package desafio_API_Cloud.core;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
